daa86a90c98d692e03bb1ec9211df92dca5247af
raeiverse <yamishiro77@gmail.com>
2022-04-07 18:16:22 +0700

    step 1 + 2
    input assets fonts & images

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset('assets/images/profil.png'),
+    );
+  }
+}
