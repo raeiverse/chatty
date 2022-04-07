7ec0eb57e8df7e997722a0b736b8462edf794144
raeiverse <yamishiro77@gmail.com>
2022-04-07 23:26:18 +0700

    step 3, 4 & 5
    add theme.dart, profile picture, friends countainer

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,103 @@
+import 'package:chatty/theme.dart';
 import 'package:flutter/material.dart';
 
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/images/profil.png'),
+      backgroundColor: blueColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                'assets/images/profil.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sabrina Carpenter',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: whiteColor,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Travel Freelancer',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: lightblueColor,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Friends',
+                      style: titlestyle,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/friend.png',
+                          width: 55,
+                          height: 55,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Joshuer',
+                              style: titlestyle,
+                            ),
+                            Text(
+                              'Joshuer',
+                              style: subtextstyle,
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Text(
+                          'Now',
+                          style: subtextstyle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color whiteColor = Color(0xffffffff);
+Color blueColor = Color(0xff1f8DF5);
+Color lightblueColor = Color(0xff9ED0FF);
+Color blackColor = Color.fromARGB(255, 0, 0, 0);
+Color geryColor = Color(0xff808BA2);
+
+TextStyle titlestyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subtextstyle = TextStyle(
+  color: geryColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+);
